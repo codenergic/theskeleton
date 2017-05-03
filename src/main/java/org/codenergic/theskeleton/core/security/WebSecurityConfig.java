9a94c596716e40e2427787a2d9eb5da45244808e
dianw <dianw.aditya@gmail.com>
2017-05-04 01:11:42 +0700

    Polishing

--- src/main/java/org/codenergic/theskeleton/core/security/WebSecurityConfig.java
+++ src/main/java/org/codenergic/theskeleton/core/security/WebSecurityConfig.java
@@ -23,7 +23,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -49,12 +48,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userService)
 				.passwordEncoder(passwordEncoder);
-	}
-
-	@Override
-	public void configure(WebSecurity web) throws Exception {
-//		web.ignoring()
-//			.antMatchers("/api/**");
 	}
 
 	@Bean
