0eb0a638b6513391fd2a50b50e2a90d3bed9cd2a
dianw <dianw.aditya@gmail.com>
2017-07-20 00:59:18 +0700

    Add registration UI

---
+++ src/main/java/org/codenergic/theskeleton/registration/RegistrationController.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenergic.theskeleton.registration;
+
+import javax.validation.Valid;
+
+import org.codenergic.theskeleton.user.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/registration")
+public class RegistrationController {
+	private static final String REGISTRATION = "registration";
+	private UserService userService;
+
+	public RegistrationController(UserService userService) {
+		this.userService = userService;
+	}
+
+	@GetMapping
+	public String registrationView(RegistrationForm registrationForm) {
+		return REGISTRATION;
+	}
+
+	@PostMapping
+	public String register(@Valid RegistrationForm registrationForm, BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			return registrationView(registrationForm);
+		}
+		// TODO: register user
+		return REGISTRATION;
+	}
+}
---
+++ src/main/java/org/codenergic/theskeleton/registration/RegistrationForm.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenergic.theskeleton.registration;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import org.codenergic.theskeleton.core.web.ValidationConstants;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@SuppressWarnings("serial")
+public class RegistrationForm implements Serializable {
+	@JsonProperty
+	@Pattern(regexp = ValidationConstants.USERNAME_REGEX, message = "Username must be lower case alphanumeric")
+	@Size(min = 6, max = 100, message = "Username must be at least 6 character")
+	private String username;
+	@JsonProperty
+	@Size(min = 8, max = 100, message = "Password must be at least 8 character")
+	private String password;
+	@JsonProperty
+	@Pattern(regexp = ValidationConstants.EMAIL_REGEX, message = "Not a valid email address")
+	private String email;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
