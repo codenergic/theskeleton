41dbb2292c900fb77538f27eadfafed7a5bee8c4
dianw <dianw.aditya@gmail.com>
2017-08-09 14:15:05 +0700

    #10: Move async config class to scheduling package

--- src/main/java/org/codenergic/theskeleton/Application.java
+++ src/main/java/org/codenergic/theskeleton/Application.java
@@ -15,27 +15,12 @@
  */
 package org.codenergic.theskeleton;
 
-import java.util.concurrent.Executor;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @SpringBootApplication
-@EnableAsync
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	}
-	
-	@Bean
-	public Executor asyncExecutor() {
-		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-		executor.setMaxPoolSize(10);
-		executor.setThreadNamePrefix("EmailService-");
-		executor.initialize();
-		return executor;
 	}
 }
