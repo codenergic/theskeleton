ddbe07a15be76e43c00560135ac9c65b468210cc
dianw <dianw.aditya@gmail.com>
2017-09-13 00:49:16 +0700

    #18: Add s3 subdirectory for each user

--- src/main/java/org/codenergic/theskeleton/user/profile/impl/ProfileServiceImpl.java
+++ src/main/java/org/codenergic/theskeleton/user/profile/impl/ProfileServiceImpl.java
@@ -1,11 +1,6 @@
 package org.codenergic.theskeleton.user.profile.impl;
 
-import java.io.InputStream;
-import java.time.Instant;
-import java.util.UUID;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-
+import io.minio.MinioClient;
 import org.apache.commons.lang3.StringUtils;
 import org.codenergic.theskeleton.user.UserEntity;
 import org.codenergic.theskeleton.user.UserRepository;
@@ -17,12 +12,16 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import io.minio.MinioClient;
+import java.io.InputStream;
+import java.time.Instant;
+import java.util.UUID;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @Transactional
 public class ProfileServiceImpl implements ProfileService {
-	private static final String PICTURE_BUCKET_NAME = "profile.pictures";
+	private static final String PICTURE_BUCKET_NAME = "profile-pictures";
 	private final Logger logger = LoggerFactory.getLogger(getClass());
 	private UserRepository userRepository;
 	private MinioClient minioClient;
@@ -74,7 +73,7 @@ public class ProfileServiceImpl implements ProfileService {
 	@Override
 	public UserEntity updateProfilePicture(String username, InputStream image, String contentType) throws Exception {
 		UserEntity user = findProfileByUsername(username);
-		String imageObjectName = StringUtils.join(user.getId(), "-", Long.toHexString(Instant.now().toEpochMilli()),
+		String imageObjectName = StringUtils.join(user.getId(), "/", Long.toHexString(Instant.now().toEpochMilli()),
 				"-", UUID.randomUUID().toString());
 		minioClient.putObject(PICTURE_BUCKET_NAME, imageObjectName, image, contentType);
 		user.setPictureUrl(minioClient.getObjectUrl(PICTURE_BUCKET_NAME, imageObjectName));
