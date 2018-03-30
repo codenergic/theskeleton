931b4bdabe5d49647ef917cdfcd1dbae38360e3c
dianw <dianw.aditya@gmail.com>
2018-03-30 19:12:33 +0700

    Resolve #66: Define s3 bucket policy

--- src/main/java/org/codenergic/theskeleton/core/data/S3ClientConfig.java
+++ src/main/java/org/codenergic/theskeleton/core/data/S3ClientConfig.java
@@ -18,6 +18,8 @@ package org.codenergic.theskeleton.core.data;
 import io.minio.MinioClient;
 import io.minio.errors.InvalidEndpointException;
 import io.minio.errors.InvalidPortException;
+import io.minio.policy.PolicyType;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -25,32 +27,43 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
 @Configuration
-public class S3ClientConfig {
+class S3ClientConfig {
 	private final Logger logger = LoggerFactory.getLogger(getClass());
 
 	@Bean
 	public ScheduledFuture<List<String>> createBuckets(MinioClient minioClient, ScheduledExecutorService executorService, S3ClientProperties clientProps) {
 		return executorService.schedule(() -> {
 			try {
-				for (String bucket : clientProps.buckets) {
-					logger.info("Checking bucket: {}", bucket);
-					if (minioClient.bucketExists(bucket))
-						continue;
-					logger.info("Bucket doesn't exist, creating one");
-					minioClient.makeBucket(bucket);
+				List<String> bucketNames = new ArrayList<>();
+				for (S3BucketProperties bucket : clientProps.buckets) {
+					bucketNames.add(bucket.name);
+					logger.info("Checking bucket [{}]", bucket.name);
+					if (!minioClient.bucketExists(bucket.name)) {
+						logger.info("Bucket doesn't exists, creating one");
+						minioClient.makeBucket(bucket.name);
+						logger.info("Bucket created");
+					} else {
+						logger.info("Bucket already exists");
+					}
+					for (S3BucketPolicyProperties policy : bucket.getPolicies()) {
+						if (policy.policy == null || StringUtils.isBlank(policy.prefix))
+							continue;
+						logger.info("Setting policy [{}] to bucket [{}] with prefix [{}]", policy.policy, bucket.name, policy.prefix);
+						minioClient.setBucketPolicy(bucket.name, policy.prefix, policy.policy);
+					}
 				}
+				return Collections.unmodifiableList(bucketNames);
 			} catch (Exception e) {
 				logger.error(e.getMessage(), e);
-			} finally {
-				logger.info("Bucket successfully created");
 			}
-			return clientProps.buckets;
+			return Collections.emptyList();
 		}, 5, TimeUnit.SECONDS);
 	}
 
@@ -65,13 +78,13 @@ public class S3ClientConfig {
 		return new S3ClientProperties();
 	}
 
-	protected static class S3ClientProperties {
+	public static class S3ClientProperties {
 		private String accessKey;
-		private List<String> buckets = new ArrayList<>();
+		private List<S3BucketProperties> buckets = new ArrayList<>();
 		private String endpoint;
 		private String secretKey;
 
-		public List<String> getBuckets() {
+		public List<S3BucketProperties> getBuckets() {
 			return buckets;
 		}
 
@@ -85,6 +98,35 @@ public class S3ClientConfig {
 
 		public void setSecretKey(String secretKey) {
 			this.secretKey = secretKey;
+		}
+	}
+
+	public static class S3BucketProperties {
+		private String name;
+		private List<S3BucketPolicyProperties> policies = new ArrayList<>();
+
+		public List<S3BucketPolicyProperties> getPolicies() {
+			return policies;
+		}
+
+		public S3BucketProperties setName(String name) {
+			this.name = name;
+			return this;
+		}
+	}
+
+	public static class S3BucketPolicyProperties {
+		private PolicyType policy;
+		private String prefix = "*";
+
+		public S3BucketPolicyProperties setPolicy(PolicyType policy) {
+			this.policy = policy;
+			return this;
+		}
+
+		public S3BucketPolicyProperties setPrefix(String prefix) {
+			this.prefix = prefix;
+			return this;
 		}
 	}
 }
