e58d7158216a3718fe6af07687a8d4d02fd99b42
dianw <dianw.aditya@gmail.com>
2018-06-18 15:30:22 +0700

    Make coverage happy

--- src/test/java/org/codenergic/theskeleton/client/OAuth2ClientServiceTest.java
+++ src/test/java/org/codenergic/theskeleton/client/OAuth2ClientServiceTest.java
@@ -15,6 +15,7 @@
  */
 package org.codenergic.theskeleton.client;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Optional;
 
@@ -24,11 +25,16 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.Spy;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.provider.ClientRegistrationException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -46,11 +52,48 @@ public class OAuth2ClientServiceTest {
 	}
 
 	@Test
+	public void testDeleteClient() {
+		OAuth2ClientEntity result = new OAuth2ClientEntity().setName("123");
+		when(clientRepository.findById("123")).thenReturn(Optional.of(result));
+		clientService.deleteClient("123");
+		verify(clientRepository).delete(result);
+		verify(clientRepository).findById("123");
+
+		when(clientRepository.findById("1234")).thenReturn(Optional.empty());
+		assertThatThrownBy(() -> clientService.deleteClient("1234")).isInstanceOf(ClientRegistrationException.class);
+		verify(clientRepository).findById("1234");
+	}
+
+	@Test
+	public void testFindClientByOwner() {
+		Page<OAuth2ClientEntity> result = new PageImpl<>(Collections.singletonList(new OAuth2ClientEntity()));
+		when(clientRepository.findByCreatedByUserId(anyString(), any())).thenReturn(result);
+		assertThat(clientService.findClientByOwner("123", null)).isEqualTo(result);
+		verify(clientRepository).findByCreatedByUserId(anyString(), any());
+	}
+
+	@Test
+	public void testFindClients() {
+		Page<OAuth2ClientEntity> result = new PageImpl<>(Collections.singletonList(new OAuth2ClientEntity()));
+		when(clientRepository.findByNameOrDescriptionContaining(anyString(), any())).thenReturn(result);
+		assertThat(clientService.findClients("123", null)).isEqualTo(result);
+		verify(clientRepository).findByNameOrDescriptionContaining(anyString(), any());
+	}
+
+	@Test
 	public void testGenerateSecret() {
 		when(clientRepository.findById("client")).thenReturn(Optional.of(new OAuth2ClientEntity().setId("client")));
 		OAuth2ClientEntity result = clientService.generateSecret("client");
 		assertThat(passwordEncoder.matches(result.getClientId(), result.getClientSecret())).isTrue();
 		verify(clientRepository).findById("client");
+	}
+
+	@Test
+	public void testLoadClientByClientId() {
+		OAuth2ClientEntity result = new OAuth2ClientEntity().setName("123");
+		when(clientRepository.findById("123")).thenReturn(Optional.of(result));
+		assertThat(clientService.loadClientByClientId("123")).isEqualTo(result);
+		verify(clientRepository).findById("123");
 	}
 
 	@Test
--- src/test/java/org/codenergic/theskeleton/privilege/PrivilegeServiceTest.java
+++ src/test/java/org/codenergic/theskeleton/privilege/PrivilegeServiceTest.java
@@ -16,6 +16,7 @@
 package org.codenergic.theskeleton.privilege;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
@@ -23,6 +24,7 @@ import java.util.UUID;
 
 import org.codenergic.theskeleton.privilege.impl.PrivilegeServiceImpl;
 import org.codenergic.theskeleton.role.RoleEntity;
+import org.codenergic.theskeleton.role.RoleNotFoundException;
 import org.codenergic.theskeleton.role.RoleRepository;
 import org.junit.Before;
 import org.junit.Test;
@@ -34,6 +36,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
@@ -56,41 +59,6 @@ public class PrivilegeServiceTest {
 	}
 
 	@Test
-	public void testFindPrivilegeByName() {
-		PrivilegeEntity result = new PrivilegeEntity()
-			.setName("user");
-		result.getAuthority();
-		when(privilegeRepository.findByName(eq("user"))).thenReturn(Optional.of(result));
-		assertThat(privilegeService.findPrivilegeByName("user").orElse(new PrivilegeEntity())).isEqualTo(result);
-		verify(privilegeRepository).findByName(eq("user"));
-		when(privilegeRepository.findByName(eq("admin"))).thenReturn(Optional.empty());
-		assertThat(privilegeService.findPrivilegeByName("admin").isPresent()).isFalse();
-		verify(privilegeRepository).findByName(eq("admin"));
-	}
-
-	@Test
-	@SuppressWarnings("serial")
-	public void testFindPrivilegeById() {
-		PrivilegeEntity result = new PrivilegeEntity() {{ setId("123"); }}.setName("user_list_read");
-		when(privilegeRepository.findById(eq("123"))).thenReturn(Optional.of(result));
-		assertThat(privilegeService.findPrivilegeById("123").orElse(new PrivilegeEntity())).isEqualTo(result);
-		verify(privilegeRepository).findById(eq("123"));
-		when(privilegeRepository.findById(eq("124"))).thenReturn(Optional.empty());
-		assertThat(privilegeService.findPrivilegeById("124").isPresent()).isFalse();
-		verify(privilegeRepository).findById(eq("124"));
-	}
-
-	@Test
-	@SuppressWarnings("serial")
-	public void testFindPrivileges() {
-		PrivilegeEntity result = new PrivilegeEntity() {{ setId("123"); }}.setName("user_list_read");
-		Page<PrivilegeEntity> page = new PageImpl<>(Arrays.asList(result));
-		when(privilegeRepository.findAll(any(Pageable.class))).thenReturn(page);
-		assertThat(privilegeService.findPrivileges(new PageRequest(1, 10))).isEqualTo(page);
-		verify(privilegeRepository).findAll(any(Pageable.class));
-	}
-
-	@Test
 	public void testAddPrivilegeToRole() {
 		RoleEntity role = new RoleEntity()
 			.setId(UUID.randomUUID().toString())
@@ -108,13 +76,65 @@ public class PrivilegeServiceTest {
 		verify(roleRepository).findByCode("role");
 		verify(privilegeRepository).findByName("privilege");
 		verify(rolePrivilegeRepository).save(any(RolePrivilegeEntity.class));
+
+		when(roleRepository.findByCode("role1")).thenReturn(Optional.empty());
+		assertThatThrownBy(() -> {
+			privilegeService.addPrivilegeToRole("role1", "privilege");
+		}).isInstanceOf(RoleNotFoundException.class);
+		verify(roleRepository).findByCode("role1");
+
+		when(privilegeRepository.findByName("privilege1")).thenReturn(Optional.empty());
+		assertThatThrownBy(() -> {
+			privilegeService.addPrivilegeToRole("role", "privilege1");
+		}).isInstanceOf(PrivilegeNotFoundException.class);
+		verify(privilegeRepository).findByName("privilege1");
 	}
 
 	@Test
-	public void testRemovePrivilegeFromRole() {
-		when(roleRepository.findByCode(anyString())).thenReturn(Optional.of(new RoleEntity()));
-		privilegeService.removePrivilegeFromRole("", "");
-		verify(roleRepository).findByCode(anyString());
+	@SuppressWarnings("serial")
+	public void testFindPrivilegeById() {
+		PrivilegeEntity result = new PrivilegeEntity() {{ setId("123"); }}.setName("user_list_read");
+		when(privilegeRepository.findById(eq("123"))).thenReturn(Optional.of(result));
+		assertThat(privilegeService.findPrivilegeById("123").orElse(new PrivilegeEntity())).isEqualTo(result);
+		verify(privilegeRepository).findById(eq("123"));
+		when(privilegeRepository.findById(eq("124"))).thenReturn(Optional.empty());
+		assertThat(privilegeService.findPrivilegeById("124").isPresent()).isFalse();
+		verify(privilegeRepository).findById(eq("124"));
+	}
+
+	@Test
+	public void testFindPrivilegeByIdOrName() {
+		PrivilegeEntity result = new PrivilegeEntity().setName("user_listRead");
+		when(privilegeRepository.findById(eq("123"))).thenReturn(Optional.of(result));
+		assertThat(privilegeService.findPrivilegeByIdOrName("123").orElse(new PrivilegeEntity())).isEqualTo(result);
+		verify(privilegeRepository).findById(eq("123"));
+	}
+
+	@Test
+	public void testFindPrivilegeByName() {
+		PrivilegeEntity result = new PrivilegeEntity()
+			.setName("user");
+		result.getAuthority();
+		when(privilegeRepository.findByName(eq("user"))).thenReturn(Optional.of(result));
+		assertThat(privilegeService.findPrivilegeByName("user").orElse(new PrivilegeEntity())).isEqualTo(result);
+		verify(privilegeRepository).findByName(eq("user"));
+		when(privilegeRepository.findByName(eq("admin"))).thenReturn(Optional.empty());
+		assertThat(privilegeService.findPrivilegeByName("admin").isPresent()).isFalse();
+		verify(privilegeRepository).findByName(eq("admin"));
+	}
+
+	@Test
+	@SuppressWarnings("serial")
+	public void testFindPrivileges() {
+		PrivilegeEntity result = new PrivilegeEntity() {{ setId("123"); }}.setName("user_list_read");
+		Page<PrivilegeEntity> page = new PageImpl<>(Collections.singletonList(result));
+		when(privilegeRepository.findAll(any(Pageable.class))).thenReturn(page);
+		assertThat(privilegeService.findPrivileges(new PageRequest(1, 10))).isEqualTo(page);
+		verify(privilegeRepository).findAll(any(Pageable.class));
+
+		when(privilegeRepository.findByNameOrDescriptionStartsWith(anyString(), any())).thenReturn(page);
+		assertThat(privilegeService.findPrivileges("123", null)).isEqualTo(page);
+		verify(privilegeRepository).findByNameOrDescriptionStartsWith(anyString(), any());
 	}
 
 	@Test
@@ -128,5 +148,12 @@ public class PrivilegeServiceTest {
 		assertThat(result.size()).isEqualTo(1);
 		assertThat(result.iterator().next()).isEqualTo(dbResult.iterator().next().getPrivilege());
 		verify(rolePrivilegeRepository).findByRoleCode("role");
+	}
+
+	@Test
+	public void testRemovePrivilegeFromRole() {
+		when(roleRepository.findByCode(anyString())).thenReturn(Optional.of(new RoleEntity()));
+		privilegeService.removePrivilegeFromRole("", "");
+		verify(roleRepository).findByCode(anyString());
 	}
 }
